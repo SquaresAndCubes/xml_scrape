43348ccbf4969e1d2ad6081cbfe41718e951fbd0
Brent Vaalburg <btvaalburg@gmail.com>
2018-04-09 03:21:43 -0600

    V 1.0 All Data Parsed Successful

--- xml_object.py
+++ xml_object.py
@@ -6,10 +6,10 @@ import string
 
 #key generator for database PK's
 
-def show_keygen(size=5, chars=string.ascii_lowercase + string.digits):
+def show_keygen(size=7, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
-def song_keygen(size=6, chars=string.ascii_lowercase + string.digits):
+def song_keygen(size=8, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
 #config for DB connection
@@ -44,8 +44,8 @@ shows_data = (
 songs_data = (
 
     "INSERT INTO songs "
-    " (song_key, show_key, song_name, song_set, song_track, song_encore, song_segue, song_notes, song_cover) "
-    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    " (song_key, show_key, song_name, song_set, song_track, song_encore, song_segue, song_notes, song_cover, song_with_guest) "
+    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 )
 
 #mail for look scrapes XML
@@ -57,31 +57,40 @@ for setlist in root.iterchildren(tag='setlist'):
     show_city = setlist.venue.city.attrib['name']
     show_state = setlist.venue.city.attrib['stateCode']
     show_country = setlist.venue.city.country.attrib['code']
-
+    song_track = 0
     for set in setlist.sets.iterchildren(tag='set'):
-        song_track = 0
+
         for song in set.iterchildren(tag='song'):
             song_key = song_keygen()
             song_name = song.get('name')
             song_set = set.get('name')
-
+            song_track = song_track + 1
             song_encore = set.get('encore')
             song_segue = None
             song_notes = None
             song_cover = None
+            song_with_guest = None
 
-            for detail in song.iterchildren():
-                song_cover = detail.get('name')
+            for detail in song.iterchildren(tag='info'):
+
                 if detail.text == '>':
                     song_segue = detail.text
                 else:
                     song_notes = detail.text
 
+            for detaila in song.iterchildren(tag='cover'):
+                song_cover = detaila.get('name')
 
-            song_data = song_key, show_key, song_name, song_set, song_track + 1, song_encore, song_segue, song_notes, song_cover
+
+            for detailb in song.iterchildren(tag='with'):
+                song_with_guest = detailb.get('name')
+
+
+
+            song_data = song_key, show_key, song_name, song_set, song_track, song_encore, song_segue, song_notes, song_cover, song_with_guest
             sts9_db.execute(songs_data, song_data)
 
-            print(show_key, song_key, song_name, 'set: ' + str(song_set), 'track#: ' + str(song_track), 'encore: ' + str(song_encore), 'segue: ' + str(song_segue), 'notes: ' + str(song_notes), 'cover: ' +str(song_cover))
+            print(song_name, 'set: ' + str(song_set), 'track#: ' + str(song_track), 'encore: ' + str(song_encore), 'segue: ' + str(song_segue), 'notes: ' + str(song_notes), 'cover: ' +str(song_cover))
 
 
     show = show_key, datetime.strptime(show_date, '%d-%m-%Y').strftime('%Y-%m-%d'), show_venue, show_city, show_state, show_country
